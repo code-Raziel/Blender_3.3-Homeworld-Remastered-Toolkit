b4fa69cdcc5eaf4d4819cfcc92e61bc6b3ad0d44
code-Raziel <114299554+code-Raziel@users.noreply.github.com>
2023-01-18 20:51:18 +0100

    Update make_HOD.bat
    Fixed Static Path issue

--- hwrm_toolkit/HODOR/make_HOD.bat
+++ hwrm_toolkit/HODOR/make_HOD.bat
@@ -1,7 +1,13 @@
 @echo off
-echo Building hod for %1:
-SET ship_id=%1
-C:
+echo "---------------------------"
+echo " Building hod for %1"
+echo "---------------------------"
+SET "ship_id=%1"
+REM echo %1
+REM echo %2
+SET "unit=%~dp0"
+%unit:~0,2%
 CD %~dp0
-HODOR.exe -$SHIP_NAME=%ship_id% -script="HodMake.hodor"
+HODOR.exe -$SHIP_NAME=%ship_id% -$HWRM_BASE=%2 -script="HodMake.hodor"
 TIMEOUT 5
+REM PAUSE
